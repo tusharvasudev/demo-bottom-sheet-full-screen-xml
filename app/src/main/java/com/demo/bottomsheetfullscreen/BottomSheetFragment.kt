6b3bd2511816da643aca068d2496228386bab1c3
Tushar Vasudev <tusharvasudev@outlook.com>
2021-09-25 13:02:00 +0530

    full screen and cancel button

--- app/src/main/java/com/demo/bottomsheetfullscreen/BottomSheetFragment.kt
+++ app/src/main/java/com/demo/bottomsheetfullscreen/BottomSheetFragment.kt
@@ -1,5 +1,6 @@
 package com.demo.bottomsheetfullscreen
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.ContextMenu
 import androidx.fragment.app.Fragment
@@ -9,8 +10,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import android.graphics.drawable.ColorDrawable
+import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    private lateinit var dialogBox : BottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,4 +25,28 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
     }
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        dialogBox = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialogBox.setCancelable(false)
+        dialogBox.setCanceledOnTouchOutside(false)
+        dialogBox.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        dialogBox.behavior.isDraggable = false
+        return dialogBox
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cancelButton = view.findViewById<Button>(R.id.cancel_button)
+        cancelButton.setBackgroundColor(resources.getColor(R.color.design_default_color_primary))
+        cancelButton.setTextColor(resources.getColor(R.color.design_default_color_on_primary))
+
+        cancelButton.setOnClickListener {
+            dialogBox.cancel()
+        }
+    }
+
 }
